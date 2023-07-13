536ed9c1f403bb1133cdcf21a5f6bed12e7d078e
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-14 01:21:58 +0530

    feat: add analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+import { inject } from '@vercel/analytics';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
