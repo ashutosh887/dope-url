6f50d3870e69c1b5850d13c33f4aa78912a09d82
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-13 22:34:20 +0530

    test: rm redirect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomeContainer from "./container/HomeContainer";
-import RedirectContainer from "./container/RedirectContainer";
+// import RedirectContainer from "./container/RedirectContainer";
 import NotFound from "./container/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeContainer />} />
-      <Route path="/:shortId" element={<RedirectContainer />} />
+      {/* <Route path="/:shortId" element={<RedirectContainer />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
