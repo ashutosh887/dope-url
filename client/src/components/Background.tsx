597f7c135ed113ac2bb8f3ba49b2b858c0415556
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-13 21:27:55 +0530

    feat: update bg

--- client/src/components/Background.tsx
+++ client/src/components/Background.tsx
@@ -1,39 +1,16 @@
-import { useEffect, useState } from "react";
-import { Image } from "@chakra-ui/react";
-
-function getWindowDimentions() {
-  const { innerWidth: width, innerHeight: height } = window;
-  return {
-    width,
-    height,
-  };
-}
+import bgImage from "../assets/bg.jpg";
 
 function Background() {
-  const [dimensions, setDimensions] = useState(getWindowDimentions());
-
-  useEffect(() => {
-    function handleResize() {
-      setDimensions(getWindowDimentions());
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const img = `https://source.unsplash.com/random/${dimensions.width}x${dimensions.height}`;
-
   return (
-    <Image
-      position="fixed"
-      top="0"
-      left="0"
-      bottom="0"
-      right="0"
-      zIndex="1"
-      src={img}
-      alt="bg"
+    <img
+      src={bgImage}
+      style={{
+        height: "100%",
+        width: "100%",
+        objectFit: "cover",
+        zIndex: 1,
+        position: "fixed",
+      }}
     />
   );
 }
