fe1bfccfc14ea0210954fc70b691e1d60e3888a3
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-13 21:28:23 +0530

    feat: upgrade redirection logic

--- client/src/components/URLForm.tsx
+++ client/src/components/URLForm.tsx
@@ -1,23 +1,34 @@
-import { Box, Input, InputGroup, Button } from "@chakra-ui/react";
+import { Box, Input, InputGroup, Button, Tooltip } from "@chakra-ui/react";
 import { FormEvent, useState } from "react";
 import { SERVER_ENDPOINT } from "../config";
 import axios from "axios";
+import { CopyIcon } from "@chakra-ui/icons";
 
 const URLForm = () => {
   const [destination, setDestination] = useState<string>("");
-  const [shortUrl, setShortUrl] = useState<{
-    shortId: string;
-  } | null>(null);
+  const [shortUrl, setShortUrl] = useState<string>("");
+  const [isGenerating, setIsGenerating] = useState<boolean>(false);
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    setIsGenerating(!isGenerating);
     event.preventDefault();
-    setShortUrl(null);
-    const result = await axios
+    setShortUrl("");
+
+    await axios
       .post(`${SERVER_ENDPOINT}/api/url`, {
         destination: destination,
       })
-      .then((response) => response.data);
-    setShortUrl(result);
+      .then((response) => {
+        response.data;
+        setShortUrl(`${window.location.origin}/${response?.data?.shortId}`);
+      })
+      .catch((error) => {
+        alert(error.response.data.errors["0"]);
+        setShortUrl("");
+        setIsGenerating(false);
+      });
+
+    setIsGenerating(false);
   }
 
   return (
@@ -28,17 +39,41 @@ const URLForm = () => {
             onChange={(event) => setDestination(event.target.value)}
             placeholder="https://ashutosh887.vercel.app"
             width="300px"
+            margin="2px"
           />
-          <Button type="submit" disabled={true}>
+          <Button
+            type="submit"
+            margin="2px"
+            colorScheme="telegram"
+            isLoading={isGenerating}
+            spinnerPlacement="start"
+          >
             Create
           </Button>
         </InputGroup>
       </form>
 
       {shortUrl && (
-        <a href={`/${shortUrl?.shortId}`} target="_blank">
-          {window.location.origin}/{shortUrl?.shortId}
-        </a>
+        <Box
+          padding="2"
+          width="100%"
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <a href={shortUrl} target="_blank">
+            {shortUrl}
+          </a>
+
+          <Tooltip label="Copy">
+            <Button
+              size="sm"
+              onClick={() => navigator.clipboard.writeText(shortUrl)}
+            >
+              <CopyIcon />
+            </Button>
+          </Tooltip>
+        </Box>
       )}
     </Box>
   );
--- client/src/container/RedirectContainer.tsx
+++ client/src/container/RedirectContainer.tsx
@@ -1,26 +1,55 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { SERVER_ENDPOINT } from "../config";
 import axios from "axios";
-import { Box, Spinner } from "@chakra-ui/react";
+import { Box, Spinner, Text } from "@chakra-ui/react";
+import Background from "../components/Background";
 
 function RedirectContainer() {
-  const [destination, setDestination] = useState<null | string>(null);
-  const [error, setError] = useState();
+  const [destination, setDestination] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
   const { shortId } = useParams();
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     async function getData() {
-      return await axios
-        .get(`${SERVER_ENDPOINT}/api/url/${shortId}`)
-        .then((res) => setDestination(res.data?.destination))
-        .catch((error) => setError(error.message));
+      try {
+        const response = await axios.get(
+          `${SERVER_ENDPOINT}/api/url/${shortId}`
+        );
+        const { data } = response;
+        setDestination(data?.destination);
+      } catch (error) {
+        setError(JSON.stringify(error));
+      } finally {
+        setLoading(false);
+      }
     }
+
     getData();
   }, [shortId]);
 
-  if (!destination && !error) {
+  useEffect(() => {
+    if (destination) {
+      window.location.href = destination;
+      // Replace the current history entry with a new one
+      window.history.replaceState(null, "", window.location.href);
+    } else if (!loading && error) {
+      // Redirect to the homepage after a delay
+      const redirectTimeout = setTimeout(() => {
+        navigate("/");
+      }, 2500);
+
+      return () => {
+        clearTimeout(redirectTimeout);
+      };
+    }
+  }, [destination, loading, error, navigate]);
+
+  if (loading) {
     return (
       <Box
         height="100%"
@@ -33,7 +62,35 @@ function RedirectContainer() {
     );
   }
 
-  return <p>{error && JSON.stringify(error)}</p>;
+  if (error) {
+    return (
+      <Box
+        height="100%"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Box
+          pos="relative"
+          zIndex="2"
+          padding="6"
+          border="2px solid white"
+          rounded="lg"
+          backgroundColor="rgba(0,0,0,0.3)"
+        >
+          <Text fontSize="xl" fontWeight={400} color="white">
+            No such route exists!
+          </Text>
+          <Text fontSize="2xl" fontWeight={600} color="white">
+            Redirecting to homepage...
+          </Text>
+        </Box>
+        <Background />
+      </Box>
+    );
+  }
+
+  return null; // You can customize the component to render when there is no destination or error
 }
 
 export default RedirectContainer;
