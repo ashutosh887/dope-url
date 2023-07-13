996572b0cdca9b1eedc455f311851abd51a06c7e
Ashutosh Jha <ashutoshj887@gmail.com>
2023-07-13 23:55:30 +0530

    chore: rm id

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import routes from "./config/routes";
-import ShortID from "./pages/ShortID";
+// import ShortID from "./pages/ShortID";
 
 function App() {
   return (
     <Routes>
       <Route path={routes.HOME} element={<Home />} />
-      <Route path={routes.SHORT_ID} element={<ShortID />} />
+      {/* <Route path={routes.SHORT_ID} element={<ShortID />} /> */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
